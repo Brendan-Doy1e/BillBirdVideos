b92825303102a00cadc42737e86354b6d56aa5c8
Brendan-Doy1e <brendan-doyle@hotmail.com>
2023-01-12 09:09:33 -0800

    Cleaned files

---
+++ Chapter_1/using_variables.c
@@ -0,0 +1,21 @@
+/* Learning to use variables
+
+   Brendan Doyle
+
+*/
+
+#include <stdio.h>
+
+int main() {
+
+  int i;
+  i = 6; //only get 9 digits with longs
+  float g; // also called a real value
+  g = 10; 
+  
+  printf("Task 2:\n");
+  printf("The value of i is %d\nThe value of g is %.2f\n", i, g);
+
+  return 0;
+
+}

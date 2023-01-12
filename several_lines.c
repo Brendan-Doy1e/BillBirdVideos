b910f71f5674c6cdbc9f3db23a090d815cbef6e0
Brendan-Doy1e <brendan-doyle@hotmail.com>
2023-01-11 19:34:40 -0800

    Completed task 1, 2, 3

--- several_lines.c
+++ several_lines.c
@@ -1,3 +1,4 @@
+//Task 1
 /* Practicing several lines of C
    
    Brendan Doyle 11/01/2023
@@ -8,9 +9,18 @@
 
 int main() {
 
+  //Task 2
   printf("hello world!\n");
   printf("This is very hard.\n");
   printf("Why isnt my git repo working.\n");
+  printf("\tHere is some more text.\n");
+  printf("\\Here is the backslash.\n");
+  printf("Here is a quotation mark \" \n");
+
+  //Task 3
+  printf("187 x 111 is %d\n", 187 * 111);
+  printf("Six + ten is %d, 17 + 187 is %d\n", 6 +10, 17 + 187);
+  printf("The number is %d\n", 20/5);
 
   return 0;
 
